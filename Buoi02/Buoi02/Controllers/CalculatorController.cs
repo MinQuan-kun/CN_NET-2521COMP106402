1e6f9d78fe6f5e7bd0a15d7c7209146fb7d52c6c
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-01-28 16:01:52 +0700

    Demo Calculator

---
+++ Buoi02/Buoi02/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi02.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Calculate(double SoHang01 = 0, double SoHang02 = 0, char op = '+')
+        {
+            switch (op)
+            {
+                case '+':
+                    ViewBag.KetQua = SoHang01 + SoHang02;
+                    break;
+                case '-':
+                    ViewBag.KetQua = SoHang01 - SoHang02;
+                    break;
+                case 'x':
+                    ViewBag.KetQua = SoHang01 * SoHang02;
+                    break;
+                case ':':
+                    ViewBag.KetQua = SoHang01 / SoHang02;
+                    break;
+            }
+            ViewBag.So01 = SoHang01;
+            ViewBag.So02 = SoHang02;
+            ViewBag.PhepToan = op;
+            ViewBag.KetQua = ViewBag.KetQua;
+            return View("Index");
+        }
+    }
+}
---
+++ Buoi02/Buoi02/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi02.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ABC()
+        {
+            return Redirect("/Home/Privacy");
+        }
+        
+        public IActionResult XYZ()
+        {
+            return RedirectToAction("Privacy", "Home");
+        }
+
+        public IActionResult MyAPI()
+        {
+            return Json(new { Name = "Nguyen Van A", Age = 30 });
+        }
+    }
+}
