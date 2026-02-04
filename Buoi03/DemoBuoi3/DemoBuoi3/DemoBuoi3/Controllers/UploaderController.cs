88608860757dc08a4ddc47076f6caebe683198e7
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-02-04 17:10:05 +0700

    Uploadfile

---
+++ Buoi03/DemoBuoi3/DemoBuoi3/DemoBuoi3/Controllers/UploaderController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoBuoi3.Controllers
+{
+    public class UploaderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile myfile)
+        {
+            if(myfile == null)
+            {
+                ViewBag.Message = "Chưa chọn file để upload";
+            }
+            else
+            {
+                var fullpath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","data", myfile.FileName);
+                using (var stream = new FileStream(fullpath, FileMode.Create))
+                {
+                    myfile.CopyTo(stream);
+                }
+            }
+            return View("Index");
+        }
+        public IActionResult UploadFiles(List<IFormFile> myfiles)
+        {
+            if (myfiles == null || myfiles.Count == 0)
+            {
+                ViewBag.Message = "Chưa chọn file để upload";
+            }
+            else
+            {
+                foreach (var myfile in myfiles)
+                {
+                    var fullpath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", myfile.FileName);
+                    using (var stream = new FileStream(fullpath, FileMode.Create))
+                    {
+                        myfile.CopyTo(stream);
+                    }
+                }
+
+            }
+            return View("Index");
+
+        }
+    }
+}
