dd0f30149e4ea80745b9142beeee0afd8d9f7d73
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-02-04 16:31:41 +0700

    Sync

---
+++ Buoi03/DemoBuoi3/DemoBuoi3/DemoBuoi3/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using DemoBuoi3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoBuoi3.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult DemoSync()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Demo.A();
+            Demo.B();
+            Demo.C();
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds}ms");
+        }
+
+        public async Task<IActionResult> DemoAS()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var a = Demo.AA();
+            var b = Demo.BB();
+            var c = Demo.CC();
+            await a; await b; await c;
+            //Task.WaitAll();
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds}ms");
+        }
+    }
+}
