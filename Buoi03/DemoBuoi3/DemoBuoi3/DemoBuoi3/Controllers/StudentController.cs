da92fa561a421c8799b013e56338415491eb2d1e
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-02-05 21:46:07 +0700

    hoàn thành bài tập buổi 3

---
+++ Buoi03/DemoBuoi3/DemoBuoi3/DemoBuoi3/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using DemoBuoi3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoBuoi3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student model, IFormFile Hinh)
+        {
+            if (Hinh != null)
+            {
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", Hinh.FileName);
+                using (var f = new FileStream(fullPath, FileMode.CreateNew))
+                {
+                    Hinh.CopyTo(f);
+                    model.Image = Hinh.FileName;
+                }
+            }
+            return View("Profile", model);
+        }
+
+
+    }
+}
---
+++ Buoi03/DemoBuoi3/DemoBuoi3/DemoBuoi3/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoBuoi3.Models
+{
+    public class Student
+    {
+        [RegularExpression(@"\d{2}.01.104.\d{3}")]
+        public int Id { get; set; }
+        [MinLength(5, ErrorMessage = "Tên phải có ít nhất 5 ký tự")]
+        public string Name { get; set; }
+        public string Image { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Điểm từ 0 ... 10")]
+        public double Score { get; set; }
+
+    }
+}
